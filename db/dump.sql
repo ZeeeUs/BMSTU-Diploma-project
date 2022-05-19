22da8ab87acde455a451146bcaf05398891d1270
Pogosyan Sos <pogosyan.sos@wb.ru>
2022-05-20 02:03:43 +0300

    Add create schema command to sql dump file

--- db/dump.sql
+++ db/dump.sql
@@ -1,4 +1,6 @@
-CREATE TABLE users
+CREATE schema dashboard;
+
+CREATE TABLE dashboard.users
 (
     id          serial PRIMARY KEY NOT NULL,
     password    varchar(50)        NOT NULL,
@@ -8,20 +10,20 @@ CREATE TABLE users
     lastname    varchar(255)       NOT NULL,
     email       varchar unique     not null
 );
-CREATE TABLE groups
+CREATE TABLE dashboard.groups
 (
     id         serial PRIMARY KEY NOT NULL,
     group_code varchar(25)        not null
 );
 
-CREATE TABLE students
+CREATE TABLE dashboard.students
 (
     id       serial PRIMARY KEY NOT NULL,
     user_id  int2               not null,
     group_id int2               not null
 );
 
-CREATE TABLE events
+CREATE TABLE dashboard.events
 (
     id         serial PRIMARY KEY NOT NULL,
     course_id  int                not null,
@@ -30,7 +32,7 @@ CREATE TABLE events
     deadline   date
 );
 
-CREATE TABLE student_event
+CREATE TABLE dashboard.student_event
 (
     id           serial PRIMARY KEY NOT NULL,
     student_id   int                not null,
@@ -46,67 +48,67 @@ CREATE TABLE supervisors
     user_id int2               not null
 );
 
-CREATE TABLE supervisors_courses
+CREATE TABLE dashboard.supervisors_courses
 (
     course_id     int not null,
     supervisor_id int not null
 );
 
-CREATE TABLE courses
+CREATE TABLE dashboard.courses
 (
     id          serial PRIMARY KEY NOT NULL,
     semester    int                not null,
     course_name varchar(255)       not null
 );
 
-CREATE TABLE events_eventsName
+CREATE TABLE dashboard.events_eventsName
 (
     event_id        int not null,
     "event-name_id" int not null
 );
 
-CREATE TABLE events_names
+CREATE TABLE dashboard.events_names
 (
     id   serial PRIMARY KEY NOT NULL,
     name varchar(255)       not null
 );
 
-CREATE TABLE event_status
+CREATE TABLE dashboard.event_status
 (
     id          serial PRIMARY KEY NOT NULL,
     status_name varchar(255)       not null
 );
 
-CREATE TABLE group_course
+CREATE TABLE dashboard.group_course
 (
     group_id  int not null,
     course_id int not null
 );
 
-CREATE TABLE comments
+CREATE TABLE dashboard.comments
 (
     id            serial primary key not null,
     comment_field text
 );
 
-alter table students add foreign key (user_id) references users (id) on delete cascade;
-alter table students add   foreign key (group_id) references groups (id) on delete set null;
+alter table dashboard.students add foreign key (user_id) references dashboard.users (id) on delete cascade;
+alter table dashboard.students add   foreign key (group_id) references dashboard.groups (id) on delete set null;
 
-alter table events add foreign key (course_id) references courses (id) on delete cascade;
+alter table dashboard.events add foreign key (course_id) references dashboard.courses (id) on delete cascade;
 
-alter table student_event add foreign key (student_id) references students (id) on delete cascade;
-alter table student_event add foreign key (event_id) references events (id) on delete cascade;
+alter table dashboard.student_event add foreign key (student_id) references dashboard.students (id) on delete cascade;
+alter table dashboard.student_event add foreign key (event_id) references dashboard.events (id) on delete cascade;
 
-alter table supervisors add foreign key (user_id) references users (id) on delete cascade;
+alter table dashboard.supervisors add foreign key (user_id) references dashboard.users (id) on delete cascade;
 
-alter table supervisors_courses add foreign key (course_id) references courses (id) on delete cascade;
-alter table supervisors_courses add foreign key (supervisor_id) references supervisors (id) on delete cascade;
+alter table dashboard.supervisors_courses add foreign key (course_id) references dashboard.courses (id) on delete cascade;
+alter table dashboard.supervisors_courses add foreign key (supervisor_id) references dashboard.supervisors (id) on delete cascade;
 
-alter table supervisors_courses add foreign key (course_id) references courses (id) on delete cascade;
-alter table supervisors_courses add foreign key (supervisor_id) references supervisors (id) on delete cascade;
+alter table dashboard.supervisors_courses add foreign key (course_id) references dashboard.courses (id) on delete cascade;
+alter table dashboard.supervisors_courses add foreign key (supervisor_id) references dashboard.supervisors (id) on delete cascade;
 
-alter table events_eventsName add foreign key (event_id) references events (id) on delete cascade;
-alter table events_eventsName add foreign key ("event-name_id") references events_names (id) on delete cascade;
+alter table dashboard.events_eventsName add foreign key (event_id) references dashboard.events (id) on delete cascade;
+alter table dashboard.events_eventsName add foreign key ("event-name_id") references dashboard.events_names (id) on delete cascade;
 
-alter table group_course add foreign key (group_id) references groups (id) on delete cascade;
-alter table group_course add foreign key (course_id) references courses (id) on delete cascade;
+alter table dashboard.group_course add foreign key (group_id) references dashboard.groups (id) on delete cascade;
+alter table dashboard.group_course add foreign key (course_id) references dashboard.courses (id) on delete cascade;
